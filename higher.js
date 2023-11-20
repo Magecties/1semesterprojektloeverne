be4228e08fed235af59d60fe2a3d6b1c8b7efec0
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2023-11-20 14:05:42 +0100

    commit til gruppe fordi jeg hader jer dumme mega

--- higher.js
+++ higher.js
@@ -1,3 +1,9 @@
 d3.json("http://localhost:3000/skrald").then(function(d){
     console.log(d.skrald[10]);
+
+    console.log(d.skrald[10].nedbrydningstidværdi < d.skrald[11].nedbrydningstidværdi);
 });
+
+
+// d.skrald[10].navn fjern herfra
+// kør både node main.js og liveserver i to terminaler
