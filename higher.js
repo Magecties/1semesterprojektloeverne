be56c165cc4ef8867a4c9c42137fd08402a7b137
FabianSteinbock <fabiansteinbock1@gmail.com>
2023-11-22 11:31:39 +0100

    :)

--- higher.js
+++ higher.js
@@ -10,29 +10,29 @@ d3.json("http://localhost:3000/skrald").then(function(d){
 // lave det her om til et loop på sigt og få sat Number.parseInt den her ind i loopet og fjern det fra checkAnswer funktionen. 
 //Billederne skal nok i et array?
 const materials = [
-    { name: d.skrald[1].navn, image: '/Spilbilleder/Cigaretskoder.png', nedbrydningstid: d.skrald[1].nedbrydningstid, nedbrydningstidværdi: d.skrald[1].nedbrydningstidværdi },
-    { name:  d.skrald[2].navn, image: '/Spilbilleder/Tyggegummi.png', nedbrydningstid: d.skrald[2].nedbrydningstid, nedbrydningstidværdi: d.skrald[2].nedbrydningstidværdi },
-    { name:  d.skrald[3].navn, image: '/Spilbilleder/ParpirPoser.png', nedbrydningstid: d.skrald[3].nedbrydningstid, nedbrydningstidværdi: d.skrald[3].nedbrydningstidværdi },
-    { name:  d.skrald[4].navn, image: '/Spilbilleder/PlastikPoser.png', nedbrydningstid: d.skrald[4].nedbrydningstid, nedbrydningstidværdi: d.skrald[4].nedbrydningstidværdi },
-    { name:  d.skrald[5].navn, image: '/Spilbilleder/Hundelort.png', nedbrydningstid: d.skrald[5].nedbrydningstid, nedbrydningstidværdi: d.skrald[5].nedbrydningstidværdi },
-    { name:  d.skrald[6].navn, image: '/Spilbilleder/Aluminiumsdåser.png', nedbrydningstid: d.skrald[6].nedbrydningstid, nedbrydningstidværdi: d.skrald[6].nedbrydningstidværdi },
-    { name:  d.skrald[7].navn, image: '/Spilbilleder/PlastikFlasker.png', nedbrydningstid: d.skrald[7].nedbrydningstid, nedbrydningstidværdi: d.skrald[7].nedbrydningstidværdi },
-    { name:  d.skrald[8].navn, image: '/Spilbilleder/Glas.png', nedbrydningstid: d.skrald[8].nedbrydningstid, nedbrydningstidværdi: d.skrald[8].nedbrydningstidværdi },
-    { name:  d.skrald[9].navn, image: '/Spilbilleder/Ballon.png', nedbrydningstid: d.skrald[9].nedbrydningstid, nedbrydningstidværdi: d.skrald[9].nedbrydningstidværdi },
-    { name:  d.skrald[10].navn, image: '/Spilbilleder/Æbleskrog.png', nedbrydningstid: d.skrald[10].nedbrydningstid, nedbrydningstidværdi: d.skrald[10].nedbrydningstidværdi },
-    { name:  d.skrald[11].navn, image: '/Spilbilleder/Fiskesnøre.png', nedbrydningstid: d.skrald[11].nedbrydningstid, nedbrydningstidværdi: d.skrald[11].nedbrydningstidværdi },
-    { name:  d.skrald[12].navn, image: '/Spilbilleder/Plastkrus.png', nedbrydningstid: d.skrald[12].nedbrydningstid, nedbrydningstidværdi: d.skrald[12].nedbrydningstidværdi },
-    { name:  d.skrald[13].navn, image: '/Spilbilleder/Ispind.png', nedbrydningstid: d.skrald[13].nedbrydningstid, nedbrydningstidværdi: d.skrald[13].nedbrydningstidværdi },
-    { name:  d.skrald[14].navn, image: '/Spilbilleder/Papkrus.png', nedbrydningstid: d.skrald[14].nedbrydningstid, nedbrydningstidværdi: d.skrald[14].nedbrydningstidværdi },
-    { name:  d.skrald[15].navn, image: '/Spilbilleder/Kapsel.png', nedbrydningstid: d.skrald[15].nedbrydningstid, nedbrydningstidværdi: d.skrald[15].nedbrydningstidværdi },
-    { name:  d.skrald[16].navn, image: '/Spilbilleder/Appelsinskrald.png', nedbrydningstid: d.skrald[16].nedbrydningstid, nedbrydningstidværdi: d.skrald[16].nedbrydningstidværdi },
-    { name:  d.skrald[17].navn, image: '/Spilbilleder/Bananskrald.png', nedbrydningstid: d.skrald[17].nedbrydningstid, nedbrydningstidværdi: d.skrald[17].nedbrydningstidværdi },
-    { name:  d.skrald[18].navn, image: '/Spilbilleder/Mælkekarton.png', nedbrydningstid: d.skrald[18].nedbrydningstid, nedbrydningstidværdi: d.skrald[18].nedbrydningstidværdi },
-    { name:  d.skrald[19].navn, image: '/Spilbilleder/NylonMaterialer.png', nedbrydningstid: d.skrald[19].nedbrydningstid, nedbrydningstidværdi: d.skrald[19].nedbrydningstidværdi },
-    { name:  d.skrald[20].navn, image: '/Spilbilleder/Mobiltelefon.png', nedbrydningstid: d.skrald[20].nedbrydningstid, nedbrydningstidværdi: d.skrald[20].nedbrydningstidværdi },
-    { name:  d.skrald[21].navn, image: '/Spilbilleder/Cykel.png', nedbrydningstid: d.skrald[21].nedbrydningstid, nedbrydningstidværdi: d.skrald[21].nedbrydningstidværdi },
-    { name:  d.skrald[0].navn, image: '/Spilbilleder/møbler.png', nedbrydningstid: d.skrald[0].nedbrydningstid, nedbrydningstidværdi: d.skrald[0].nedbrydningstidværdi },
-    { name:  d.skrald[22].navn, image: '/Spilbilleder/Tøj.png', nedbrydningstid: d.skrald[22].nedbrydningstid, nedbrydningstidværdi: d.skrald[22].nedbrydningstidværdi },
+    { name: d.skrald[1].navn, image: '/Spilbilleder/Cigaretskoder.png', nedbrydningstid: d.skrald[1].nedbrydningstid, nedbrydningstidværdi: d.skrald[1].nedbrydningstidværdi, beskrivelse: d.skrald[1].beskrivelse },
+    { name:  d.skrald[2].navn, image: '/Spilbilleder/Tyggegummi.png', nedbrydningstid: d.skrald[2].nedbrydningstid, nedbrydningstidværdi: d.skrald[2].nedbrydningstidværdi, beskrivelse: d.skrald[2].beskrivelse},
+    { name:  d.skrald[3].navn, image: '/Spilbilleder/ParpirPoser.png', nedbrydningstid: d.skrald[3].nedbrydningstid, nedbrydningstidværdi: d.skrald[3].nedbrydningstidværdi, beskrivelse: d.skrald[3].beskrivelse },
+    { name:  d.skrald[4].navn, image: '/Spilbilleder/PlastikPoser.png', nedbrydningstid: d.skrald[4].nedbrydningstid, nedbrydningstidværdi: d.skrald[4].nedbrydningstidværdi, beskrivelse: d.skrald[4].beskrivelse },
+    { name:  d.skrald[5].navn, image: '/Spilbilleder/Hundelort.png', nedbrydningstid: d.skrald[5].nedbrydningstid, nedbrydningstidværdi: d.skrald[5].nedbrydningstidværdi, beskrivelse: d.skrald[5].beskrivelse },
+    { name:  d.skrald[6].navn, image: '/Spilbilleder/Aluminiumsdåser.png', nedbrydningstid: d.skrald[6].nedbrydningstid, nedbrydningstidværdi: d.skrald[6].nedbrydningstidværdi, beskrivelse: d.skrald[6].beskrivelse },
+    { name:  d.skrald[7].navn, image: '/Spilbilleder/PlastikFlasker.png', nedbrydningstid: d.skrald[7].nedbrydningstid, nedbrydningstidværdi: d.skrald[7].nedbrydningstidværdi, beskrivelse: d.skrald[7].beskrivelse },
+    { name:  d.skrald[8].navn, image: '/Spilbilleder/Glas.png', nedbrydningstid: d.skrald[8].nedbrydningstid, nedbrydningstidværdi: d.skrald[8].nedbrydningstidværdi, beskrivelse: d.skrald[8].beskrivelse },
+    { name:  d.skrald[9].navn, image: '/Spilbilleder/Ballon.png', nedbrydningstid: d.skrald[9].nedbrydningstid, nedbrydningstidværdi: d.skrald[9].nedbrydningstidværdi, beskrivelse: d.skrald[9].beskrivelse },
+    { name:  d.skrald[10].navn, image: '/Spilbilleder/Æbleskrog.png', nedbrydningstid: d.skrald[10].nedbrydningstid, nedbrydningstidværdi: d.skrald[10].nedbrydningstidværdi, beskrivelse: d.skrald[10].beskrivelse },
+    { name:  d.skrald[11].navn, image: '/Spilbilleder/Fiskesnøre.png', nedbrydningstid: d.skrald[11].nedbrydningstid, nedbrydningstidværdi: d.skrald[11].nedbrydningstidværdi, beskrivelse: d.skrald[11].beskrivelse },
+    { name:  d.skrald[12].navn, image: '/Spilbilleder/Plastkrus.png', nedbrydningstid: d.skrald[12].nedbrydningstid, nedbrydningstidværdi: d.skrald[12].nedbrydningstidværdi, beskrivelse: d.skrald[12].beskrivelse },
+    { name:  d.skrald[13].navn, image: '/Spilbilleder/Ispind.png', nedbrydningstid: d.skrald[13].nedbrydningstid, nedbrydningstidværdi: d.skrald[13].nedbrydningstidværdi, beskrivelse: d.skrald[13].beskrivelse },
+    { name:  d.skrald[14].navn, image: '/Spilbilleder/Papkrus.png', nedbrydningstid: d.skrald[14].nedbrydningstid, nedbrydningstidværdi: d.skrald[14].nedbrydningstidværdi, beskrivelse: d.skrald[14].beskrivelse },
+    { name:  d.skrald[15].navn, image: '/Spilbilleder/Kapsel.png', nedbrydningstid: d.skrald[15].nedbrydningstid, nedbrydningstidværdi: d.skrald[15].nedbrydningstidværdi, beskrivelse: d.skrald[15].beskrivelse },
+    { name:  d.skrald[16].navn, image: '/Spilbilleder/Appelsinskrald.png', nedbrydningstid: d.skrald[16].nedbrydningstid, nedbrydningstidværdi: d.skrald[16].nedbrydningstidværdi, beskrivelse: d.skrald[16].beskrivelse },
+    { name:  d.skrald[17].navn, image: '/Spilbilleder/Bananskrald.png', nedbrydningstid: d.skrald[17].nedbrydningstid, nedbrydningstidværdi: d.skrald[17].nedbrydningstidværdi, beskrivelse: d.skrald[17].beskrivelse },
+    { name:  d.skrald[18].navn, image: '/Spilbilleder/Mælkekarton.png', nedbrydningstid: d.skrald[18].nedbrydningstid, nedbrydningstidværdi: d.skrald[18].nedbrydningstidværdi, beskrivelse: d.skrald[18].beskrivelse },
+    { name:  d.skrald[19].navn, image: '/Spilbilleder/NylonMaterialer.png', nedbrydningstid: d.skrald[19].nedbrydningstid, nedbrydningstidværdi: d.skrald[19].nedbrydningstidværdi, beskrivelse: d.skrald[19].beskrivelse },
+    { name:  d.skrald[20].navn, image: '/Spilbilleder/Mobiltelefon.png', nedbrydningstid: d.skrald[20].nedbrydningstid, nedbrydningstidværdi: d.skrald[20].nedbrydningstidværdi, beskrivelse: d.skrald[20].beskrivelse },
+    { name:  d.skrald[21].navn, image: '/Spilbilleder/Cykel.png', nedbrydningstid: d.skrald[21].nedbrydningstid, nedbrydningstidværdi: d.skrald[21].nedbrydningstidværdi, beskrivelse: d.skrald[21].beskrivelse },
+    { name:  d.skrald[0].navn, image: '/Spilbilleder/møbler.png', nedbrydningstid: d.skrald[0].nedbrydningstid, nedbrydningstidværdi: d.skrald[0].nedbrydningstidværdi, beskrivelse: d.skrald[0].beskrivelse },
+    { name:  d.skrald[22].navn, image: '/Spilbilleder/Tøj.png', nedbrydningstid: d.skrald[22].nedbrydningstid, nedbrydningstidværdi: d.skrald[22].nedbrydningstidværdi, beskrivelse: d.skrald[22].beskrivelse },
     
 ];
 
@@ -50,10 +50,11 @@ function setMaterials() {
     document.getElementById('leftImage').src = leftMaterial.image;
     document.getElementById('leftMaterial').innerText = leftMaterial.name;
     document.getElementById('leftnedbrydning').innerText = leftMaterial.nedbrydningstid;
+    document.getElementById('beskrivelse').innerText = leftMaterial.beskrivelse
+
 
     document.getElementById('rightImage').src = rightMaterial.image;
     document.getElementById('rightMaterial').innerText = rightMaterial.name;
-    document.getElementById('rightnedbrydning').innerText = rightMaterial.nedbrydningstid;
 
     console.log(leftMaterial);
     console.log(rightMaterial);
@@ -93,9 +94,6 @@ function checkAnswer(choice) {
 
         document.getElementById('rightImage').src = rightMaterial.image;
         document.getElementById('rightMaterial').innerText = rightMaterial.name;
-        document.getElementById('rightnedbrydning').innerText = rightMaterial.nedbrydningstid;
-
-//hallo
     } else {
           // Incorrect answer
           alert('fucking dumme taber man');
