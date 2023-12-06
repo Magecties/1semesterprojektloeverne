d2803e71045cbc46270f062fc3c9f035d1808c06
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2023-12-06 10:31:01 +0100

    test med at ændre port til process.env.port

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const PORT = process.env.PORT || 3030;
 const { Client } = require("pg");
 let pg = require("pg");
 let queryData = null;
