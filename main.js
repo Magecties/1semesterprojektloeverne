572c0d3925b76f67b0536d16984ef918419c2138
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2023-12-06 10:55:09 +0100

    tjek

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 // const port = 3000;
+
+// ny port
 const PORT = process.env.PORT || 3030;
 const { Client } = require("pg");
 let pg = require("pg");
@@ -55,6 +57,7 @@ app.get("/skraldfarlighed", async (req, res) => {
   }
 });
 
+// ny port
 app.listen(PORT, () => {
   console.log(`magnus har fikset det og lytter på ${PORT}`);
 });
