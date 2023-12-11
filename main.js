c7ad68ccac7adc36a72ea0a0ab8517a0c3b35cf3
Magnus Bach Billeskov Brask <magnusbbb@hotmail.com.com>
2023-12-11 09:38:25 +0100

    localhost

--- main.js
+++ main.js
@@ -67,5 +67,5 @@ app.listen(PORT, () => {
 */
 
 app.listen(port, () => {
-  console.log(`fake magnus lytter på http://localhost:${port}`);
+  console.log(`fake magnus lytter på https://projektspillet-loverne.onrender.com:${port}`);
 });
